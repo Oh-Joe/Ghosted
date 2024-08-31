11af6a455fca7fc8914d694df1e7f683d8675dde
Oh-Joe <antoimn@gmail.com>
2024-08-31 21:26:36 +0800

    Initial Commit

---
+++ Ghosted/GhostedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhostedApp.swift
+//  Ghosted
+//
+//  Created by Antoine Moreau on 8/31/24.
+//
+
+import SwiftUI
+
+@main
+struct GhostedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
