73429455714652cf592604ecd8037726d1c81390
Oh-Joe <antoimn@gmail.com>
2024-10-14 21:42:02 +0800

    Auth flow somewhat working, but annoying navigation issue where a "Back" button is presented on Home, allowing user to go back further than I want. Pushing this for now and trying different approach for auth flow.

---
+++ Ghosted/Views/Auth/AuthFlowView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthFlowView.swift
+//  Ghosted
+//
+//  Created by Antoine on 2024/10/14.
+//
+
+import SwiftUI
+
+struct AuthFlowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthFlowView()
+}
