4d29b1215fc9a154658a41c63e743c2eb9ff99e3
Oh-Joe <antoimn@gmail.com>
2024-10-23 20:52:50 +0800

    small cosmetics changes.

---
+++ Ghosted/Views/Interaction Views/InteractionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  InteractionRowView.swift
+//  Ghosted
+//
+//  Created by Antoine on 2024/10/23.
+//
+
+import SwiftUI
+
+struct InteractionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InteractionRowView()
+}
